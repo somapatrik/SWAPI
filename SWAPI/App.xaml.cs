75b51e7948391624c20decebb54c647c8c08f21c
Patrik Šoma <soma.patrik@gmail.com>
2024-01-13 17:42:22 +0100

    Add project files.

---
+++ SWAPI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SWAPI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SWAPI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SWAPI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
