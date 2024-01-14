2c87de2b73f8d801ed2b0e25097897e2055f3e2d
Patrik Šoma <soma.patrik@gmail.com>
2024-01-14 13:45:36 +0100

    Filter prototype

--- SWAPI/ViewModels/MainViewModel.cs
+++ SWAPI/ViewModels/MainViewModel.cs
@@ -3,28 +3,34 @@ using SWAPI.Models;
 using SWAPI.Services;
 using SWAPI.Views;
 using System.Collections.ObjectModel;
+using System.ComponentModel.Design;
 using System.Windows.Input;
 
 namespace SWAPI.ViewModels
 {
     public class MainViewModel : PrimeViewModel
     {
+        private List<Planet> _AllPlanets;
+        private int _MaxPerPage = 10;
+
         private bool _IsLoading;
         public bool IsLoading { get=>_IsLoading; set=>SetProperty(ref _IsLoading, value); }
+
 
         private ObservableCollection<Planet> _Planets;
         public ObservableCollection<Planet> Planets { get => _Planets; set => SetProperty(ref _Planets, value); }
 
-        private List<Planet> _AllPlanets;
-
-        private int _MaxPerPage = 10;
 
         private List<int> _Pages;
         public List<int> Pages { get => _Pages; set => SetProperty(ref _Pages,value); }
 
+
         private int _SelectedPage;
         public int SelectedPage { get => _SelectedPage; set => SetProperty(ref _SelectedPage,value); }
 
+
+        private string _SearchName;
+        public string SearchName { get => _SearchName; set => SetProperty(ref _SearchName, value); }
 
         public ICommand Refresh { get; private set; }
         public ICommand DisplayPage { get; private set; }
@@ -59,7 +65,14 @@ namespace SWAPI.ViewModels
 
         private void FilterHandler()
         {
-            throw new NotImplementedException();
+            SelectedPage = 1;
+            if (string.IsNullOrEmpty(SearchName))
+                _AllPlanets = PlanetService.AllPlanets;
+            else
+                _AllPlanets = PlanetService.AllPlanets.Where(x => x.Name.ToLower().Contains(SearchName.ToLower().Trim())).ToList();
+
+
+            LoadPlanets();
         }
 
         private async void Preload()
@@ -75,9 +88,11 @@ namespace SWAPI.ViewModels
 
         private async Task DisplayPageHandler()
         {
-                Planets = new ObservableCollection<Planet>();
-                var p = _AllPlanets.Skip((_SelectedPage - 1) * _MaxPerPage).Take(_MaxPerPage);
-                p.ToList().ForEach(planet => Planets.Add(planet));
+            Planets = new ObservableCollection<Planet>();
+            IEnumerable<Planet> p;
+            p = _AllPlanets.Skip((_SelectedPage - 1) * _MaxPerPage).Take(_MaxPerPage);
+
+            p.ToList().ForEach(planet => Planets.Add(planet));
         }
 
         private async void LoadPlanets()
