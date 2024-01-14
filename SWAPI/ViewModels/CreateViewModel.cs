8868e70a0259770c915fa209afd2282e4578293d
Patrik Šoma <soma.patrik@gmail.com>
2024-01-14 16:02:33 +0100

    New planet canSave

--- SWAPI/ViewModels/CreateViewModel.cs
+++ SWAPI/ViewModels/CreateViewModel.cs
@@ -6,15 +6,34 @@ namespace SWAPI.ViewModels
 {
     public class CreateViewModel : PrimeViewModel
     {
-        public string Name { get; set; }
-        public string Terrain { get; set; }
+        private string _Name;
+        public string Name 
+        {
+            get => _Name;
+            set 
+            { 
+                _Name = value;
+                RefreshCans(); 
+            } 
+        }
+        private string _Terrain;
+        public string Terrain 
+        { 
+            get => _Terrain; 
+            set
+            {
+                _Terrain = value;
+                RefreshCans();
+            }
+        }
+
         public int Population { get; set; }
 
         public ICommand CreatePlanet { get; private set; }
 
         public CreateViewModel() 
         {
-            CreatePlanet = new Command(CreateHandler);
+            CreatePlanet = new Command(CreateHandler, CanSave);
         }
 
         private async void CreateHandler()
@@ -28,6 +47,16 @@ namespace SWAPI.ViewModels
 
             WeakReferenceMessenger.Default.Send(new Messages.CreatePlanetMessage(planet));
             await Shell.Current.Navigation.PopAsync();
+        }
+
+        private void RefreshCans()
+        {
+            ((Command)CreatePlanet).ChangeCanExecute();
+        }
+
+        private bool CanSave()
+        {
+            return !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Terrain);
         }
     }
 }
